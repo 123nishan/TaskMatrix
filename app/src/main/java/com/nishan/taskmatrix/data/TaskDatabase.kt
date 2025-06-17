63dd6c1df460654b512a0811c405fc5e362e9e6c
123nishan <nishan_98@hotmail.com>
2025-06-17 21:50:54 +0100

    feat: update data layer implementation

--- app/src/main/java/com/nishan/taskmatrix/data/TaskDatabase.kt
+++ app/src/main/java/com/nishan/taskmatrix/data/TaskDatabase.kt
@@ -1,11 +1,25 @@
 package com.nishan.taskmatrix.data
 
+import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameColumn
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
 import com.nishan.taskmatrix.data.dao.TaskDao
 import com.nishan.taskmatrix.data.model.TaskEntity
 
-@Database(entities = [TaskEntity::class], version = 1)
+@Database(
+    entities = [TaskEntity::class], version = 1,
+    exportSchema = true,
+//    autoMigrations = [
+//        AutoMigration(
+//            from = 1, to = 2, spec = TaskDatabase.TaskAutoMigration::class
+//        )
+//    ]
+)
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
+
+//    @RenameColumn(tableName = "TaskEntity", fromColumnName = "time", toColumnName = "startTime")
+//    class TaskAutoMigration : AutoMigrationSpec
 }
--- app/src/main/java/com/nishan/taskmatrix/data/model/TaskEntity.kt
+++ app/src/main/java/com/nishan/taskmatrix/data/model/TaskEntity.kt
@@ -14,6 +14,8 @@ data class TaskEntity(
     @ColumnInfo(name = "priority") val priority: Int,
     @ColumnInfo(name = "date") val date: Long?,
     @ColumnInfo(name = "time") val time: Long?,
+//    @ColumnInfo(name = "start_time") val startTime: Long?,
+//    @ColumnInfo(name = "end_time") val endTime: Long?,
     @ColumnInfo(name = "is_all_day") val isAllDay: Boolean
 )
 
@@ -22,6 +24,8 @@ fun Task.asEntity() = TaskEntity(
     description = description,
     priority = priority.level,
     date = date,
-    time = time,
+    time = startTime,
+//    startTime = startTime,
+//    endTime = endTime,
     isAllDay = isAllDay
 )
