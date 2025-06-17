bcf4782e661c8a5b4fbb6fcf889664221f4da108
123nishan <nishan_98@hotmail.com>
2025-06-17 21:49:48 +0100

    feat: add date and time picker components for task creation

--- app/src/main/java/com/nishan/taskmatrix/addtask/components/DateSelector.kt
+++ app/src/main/java/com/nishan/taskmatrix/addtask/components/DateSelector.kt
@@ -1,2 +1,57 @@
 package com.nishan.taskmatrix.addtask.components
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DateSelector(
+    selectedDate: String = "",
+    isDateSelected: Boolean,
+    onDateSelectedChange: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.CalendarToday,
+            contentDescription = null,
+            modifier = Modifier
+                .align(
+                    Alignment.CenterVertically
+                )
+        )
+        Text(
+            text = "Date",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(16.dp)
+        )
+        Spacer(modifier = Modifier.weight(1f))
+
+        FilterChip(
+            onClick = onDateSelectedChange,
+            label = {
+                Text(
+                    selectedDate,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+            },
+            selected = isDateSelected
+        )
+
+
+    }
+}
--- app/src/main/java/com/nishan/taskmatrix/addtask/components/TimePicker.kt
+++ app/src/main/java/com/nishan/taskmatrix/addtask/components/TimePicker.kt
@@ -1,2 +1,97 @@
 package com.nishan.taskmatrix.addtask.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.tooling.preview.Preview
+import com.nishan.taskmatrix.addtask.util.AddTaskEvents
+import com.nishan.taskmatrix.ui.theme.TaskMatrixTheme
+
+@Composable
+fun TimeSelector(
+    modifier: Modifier = Modifier,
+    onStartTimeSelectedChange: () -> Unit,
+    onEndTimeSelectedChange: () -> Unit,
+    isStartTimeSelected: Boolean,
+    isEndTimeSelected: Boolean,
+    selectedStartTime: String,
+    selectedEndTime: String,
+    isAllDaySelected: Boolean
+
+) {
+    AnimatedVisibility(!isAllDaySelected) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    text = "Start Time",
+                    style = MaterialTheme.typography.titleMedium,
+                )
+                FilterChip(
+                    onClick = onStartTimeSelectedChange,
+                    label = {
+                        Text(
+                            selectedStartTime,
+                            style = MaterialTheme.typography.titleMedium,
+                        )
+                    },
+                    selected = isStartTimeSelected
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    text = "End Time",
+                    style = MaterialTheme.typography.titleMedium,
+                )
+                FilterChip(
+                    onClick = onEndTimeSelectedChange,
+                    label = {
+                        Text(
+                            selectedEndTime,
+                            style = MaterialTheme.typography.titleMedium,
+                        )
+                    },
+                    selected = isEndTimeSelected
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TimeSelectorPreview() {
+    TaskMatrixTheme {
+        TimeSelector(
+            onStartTimeSelectedChange = {},
+            onEndTimeSelectedChange = {},
+            isStartTimeSelected = true,
+            isEndTimeSelected = false,
+            selectedStartTime = "10:00",
+            selectedEndTime = "12:00",
+            isAllDaySelected = false
+
+        )
+    }
+}
